c08de5dc52253d843ee28b06a00e92dc1df786d7
Haoli Yin <haoli.yin@vanderbilt.edu>
2022-08-01 12:30:06 -0500

    Fix Dice score

--- src/models/speclab_module.py
+++ src/models/speclab_module.py
@@ -39,9 +39,9 @@ class SpecLabLitModule(LightningModule):
 
         # use separate metric instance for train, val and test step
         # to ensure a proper reduction over the epoch
-        self.train_dice = Dice(ignore_index=0)
-        self.val_dice = Dice(ignore_index=0)
-        self.test_dice = Dice(ignore_index=0)
+        self.train_dice = Dice()
+        self.val_dice = Dice()
+        self.test_dice = Dice()
 
         # for logging best so far validation dice score
         self.val_dice_best = MaxMetric()
