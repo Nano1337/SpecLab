896f8baa58dc05749100ab8d2534bf3bd6b45506
Haoli Yin <haoli.yin@vanderbilt.edu>
2022-08-07 21:42:12 -0500

    Change to cpu

--- hubconf.py
+++ hubconf.py
@@ -3,17 +3,11 @@ dependencies = ['torch']
 import torch
 from demo.ASPP import SRDetectModel
 
-import urllib
-url, filename = ("https://github.com/Nano1337/SpecLab/blob/main/logs/model_checkpoints/epoch_009.ckpt?raw=true", "epoch_009.ckpt")
-try: urllib.URLopener().retrieve(url, filename)
-except: urllib.request.urlretrieve(url, filename) 
-
 def srdetect():
     model = SRDetectModel()
-    state_dict = torch.load("epoch_009.ckpt")["state_dict"]
-    for key in list(state_dict.keys()):
-        state_dict[key.replace('net.', '')] = state_dict.pop(key)
-    model.load_state_dict(state_dict, map_location=torch.device('cpu'))
+    checkpoint = torch.hub.load_state_dict_from_url('https://github.com/Nano1337/SpecLab/blob/main/logs/model_checkpoints/epoch_009.ckpt?raw=true', map_location="cpu")
+    state_dict = {key.replace("net.", ""): value for key, value in checkpoint["state_dict"].items()}
+    model.load_state_dict(state_dict)
     return model
 
 
